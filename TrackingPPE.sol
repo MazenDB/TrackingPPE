252be5a2327879d951924bc9b880478a7cc6f18a
MazenDB <mazen.dibi@hotmail.com>
2020-09-08 16:00:09 +0400

    Update TrackingPPE.sol

--- TrackingPPE.sol
+++ TrackingPPE.sol
@@ -7,7 +7,7 @@ contract Registration{
     mapping(address=>bool) manufacturers;
     mapping(address=>bool) distributors;
     mapping(address=>bool) wholesalers;
-    mapping(address=>bool) retailers;
+    mapping(address=>bool) providers;
     
     modifier onlyOwner{
       require(msg.sender == owner,
@@ -44,32 +44,32 @@ contract Registration{
         wholesalers[w]=true;
     }
     
-    function registerRetailer(address r) public onlyOwner{
-        require(!retailers[r],
-        "Retailer exists already"
+    function registerProvider(address p) public onlyOwner{
+        require(!providers[p],
+        "Provider exists already"
         );
         
-        retailers[r]=true;
+        providers[p]=true;
     }
     
     function isOwner(address o) public view returns(bool){
         return (owner==o);
     }
     
-    function manufacturerExist(address m) public view returns(bool){
+    function manufacturerExists(address m) public view returns(bool){
         return manufacturers[m];
     }
     
-    function distributorExist(address d) public view returns(bool){
+    function distributorExists(address d) public view returns(bool){
         return distributors[d];
     }
     
-    function wholesalerExist(address w) public view returns(bool){
+    function wholesalerExists(address w) public view returns(bool){
         return wholesalers[w];
     }
     
-    function retailerExist(address r) public view returns(bool){
-        return retailers[r];
+    function providerExists(address p) public view returns(bool){
+        return providers[p];
     }
     
 }
@@ -91,7 +91,7 @@ contract Lot{
     
     event LotDispatched (address productID, string productName, string materialHash, string batchNumber, string CECertificateHash);
     event OwnershipTransferred (address NewOwner, string ownerType);
-    event SaletToRetailer (address productID, address retailerAddress, uint quantitySold);
+    event SaletToProvider (address productID, address providerAddress, uint quantitySold);
     
     modifier onlyOwner{
       require(msg.sender == owner,
@@ -102,16 +102,16 @@ contract Lot{
     
 
     modifier onlyWholeSaler{
-      require(RegistrationContract.wholesalerExist(msg.sender),
+      require(RegistrationContract.wholesalerExists(msg.sender),
       "Sender not authorized."
       );
       _;
     }   
     
-    constructor(address reigstration, address ID, string memory name, string memory material, string memory batch, string memory CE, uint quantity) public{
-        RegistrationContract = Registration(reigstration);
+    constructor(address registration, address ID, string memory name, string memory material, string memory batch, string memory CE, uint quantity) public{
+        RegistrationContract = Registration(registration);
         
-        if(!RegistrationContract.manufacturerExist(msg.sender))
+        if(!RegistrationContract.manufacturerExists(msg.sender))
             revert("Sender not authorized.");
         
         owner=msg.sender;
@@ -127,31 +127,31 @@ contract Lot{
     }
     
     function transferOwnership (address newOwner) public onlyOwner{
-        if(RegistrationContract.manufacturerExist(newOwner))
+        if(RegistrationContract.manufacturerExists(newOwner))
         ownerType="Manufacturer";
-        else if(RegistrationContract.distributorExist(newOwner))
+        else if(RegistrationContract.distributorExists(newOwner))
         ownerType="Distributor";
-        else if(RegistrationContract.wholesalerExist(newOwner))
+        else if(RegistrationContract.wholesalerExists(newOwner))
         ownerType="WholeSaler";
-        else if(RegistrationContract.retailerExist(newOwner))
-        ownerType="Retailer";
+        else if(RegistrationContract.providerExists(newOwner))
+        ownerType="Provider";
         else
         revert("New Owner doesn't exist.");
         owner=newOwner;
         emit OwnershipTransferred(owner,ownerType);
     }
     
-    function sellToRetailer(address retailer, uint quantityToSell) public onlyWholeSaler{
+    function sellToProvider(address provider, uint quantityToSell) public onlyWholeSaler{
         require(remainingQuantity>=quantityToSell,
         "Not enough Items available"
         );
-        require(RegistrationContract.retailerExist(retailer),
-        "Retailer does not exist"
+        require(RegistrationContract.providerExists(provider),
+        "Provider does not exist"
         );
         
         remainingQuantity-=quantityToSell;
         
-        emit SaletToRetailer(address(this),retailer,quantityToSell);
+        emit SaletToProvider(address(this),provider,quantityToSell);
         
     } 
 
@@ -162,13 +162,19 @@ contract OrderManager{
     
     Registration RegistrationContract;
     
+    enum status{
+        Pending,
+        Accepted,
+        Rejected,
+        Received
+    }
+    
     struct order{
         address manufacturer;
         address wholesaler;
         address productID;
         uint quantity;
-        bool orderConfirmed;
-        bool orderReceived;
+        status orderStatus;
     }
     
     mapping(bytes32=>order) orders;
@@ -180,14 +186,14 @@ contract OrderManager{
     }   
     
     modifier onlyWholeSaler{
-      require(RegistrationContract.wholesalerExist(msg.sender),
+      require(RegistrationContract.wholesalerExists(msg.sender),
       "Sender not authorized."
       );
       _;
     }   
     
     modifier onlyManufacturer{
-      require(RegistrationContract.manufacturerExist(msg.sender),
+      require(RegistrationContract.manufacturerExists(msg.sender),
       "Sender not authorized."
       );
       _;
@@ -195,7 +201,7 @@ contract OrderManager{
     
     event OrderPlaced (address manufacturer, address wholesaler, address productID, uint quantity);
 
-    event OrderConfirmed (bytes32 orderID);
+    event StatusUpdated (bytes32 orderID, status newStatus);
 
     event OrderReceived (bytes32 orderID);
 
@@ -209,27 +215,33 @@ contract OrderManager{
     }
     
     function placeOrder(address productID, uint quantity, address manufacturer) public onlyWholeSaler{
+        require(RegistrationContract.manufacturerExists(manufacturer),
+        "Manufacturer address is not valid");
         bytes32 temp=keccak256(abi.encodePacked(msg.sender,now,address(this),productID));
-        orders[temp]=order(manufacturer,msg.sender,productID,quantity,false,false);
+        orders[temp]=order(manufacturer,msg.sender,productID,quantity,status.Pending);
         
         emit OrderPlaced(manufacturer,msg.sender,productID,quantity);
     }
     
-    function confirmOrder(bytes32 orderID) public onlyManufacturer{
+    function confirmOrder(bytes32 orderID, bool accepted) public onlyManufacturer{
         require(orders[orderID].manufacturer==msg.sender,
         "Sender not authorized."
         );
-        
-        orders[orderID].orderConfirmed=true;
-        
-        emit OrderConfirmed(orderID);
+        if(accepted){
+            orders[orderID].orderStatus=status.Accepted;
+        }
+        else{
+            orders[orderID].orderStatus=status.Rejected;
+        }
+        emit StatusUpdated(orderID,orders[orderID].orderStatus);
+
     }
     
-    function confirmReceiving(bytes32 orderID) public onlyWholeSaler{
+    function confirmReceived(bytes32 orderID) public onlyWholeSaler{
         require(orders[orderID].wholesaler==msg.sender,
         "Sender not authorized."
         );
-        orders[orderID].orderReceived=true;
+        orders[orderID].orderStatus=status.Received;
         
         emit OrderReceived(orderID);
     }
